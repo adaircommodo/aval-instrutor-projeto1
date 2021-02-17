92bfc68220f62f880dc041e819688475cbe4b0b4
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:18:40 -0300

    Criação de Construtor e Métodos solicitados da classe Proprietario

--- Projeto1/src/projeto1/Proprietario.java
+++ Projeto1/src/projeto1/Proprietario.java
@@ -9,5 +9,54 @@ public class Proprietario {
 	private String nome;
 	private Endereco endereco;
 	private Date dataDeNascimento;
+	
+	Proprietario(String nome, String cpf, String rg, Endereco endereco) {
+		        
+		this.setNome(nome);
+		this.setCpf(cpf);
+		this.setRg(rg);
+		this.setEndereco(endereco);
+		
+	}
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getRg() {
+		return rg;
+	}
+	
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+		
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+	
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
 		
 }
