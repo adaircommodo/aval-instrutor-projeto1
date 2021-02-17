b9275d91a04e58009eaf2583702f4b4fcac8e65c
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:30:31 -0300

    Criação da classe Projeto1Teste

---
+++ Projeto1/src/projeto1/Projeto1Teste.java
@@ -0,0 +1,13 @@
+package projeto1;
+
+public class Projeto1Teste {
+
+	public static void main(String[] args) {
+		
+		Endereco endereco = new Endereco("Rua X, 109", "Bairro Y", "Guaratinguetá", "SP", "12509-750", "-");	
+		
+		
+	}
+
+}
+
