d00c976d700123af63fc5b2442b4af31ae6efc19
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:25:06 -0300

    Criação da classe Carro

---
+++ Projeto1/src/projeto1/Carro.java
@@ -0,0 +1,24 @@
+package projeto1;
+
+public class Carro {
+	
+	private Proprietario proprietario;
+	private String modelo;
+	private String cor;
+	private Marca marca;
+	private String chassi;
+	private int	ano;
+	private int velocidadeMaxima;
+	private int velocidadeAtual;
+	private int numeroDeMarchas;
+	private int marchaAtual;
+	private int numeroDePortas;
+	private boolean cambioAutomatico;
+	private boolean tetoSolar;
+	private boolean carroLigado;
+	private double volumeDeCombustivel;
+	private double capacidadeTanqueCombustivel;
+	private double consumoPorInjecao;
+		
+
+}
