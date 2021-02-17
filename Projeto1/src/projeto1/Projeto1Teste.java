d140aa1541e50b322663caabf9dd4f252867f809
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:35:38 -0300

    Testes do Projeto1

--- Projeto1/src/projeto1/Projeto1Teste.java
+++ Projeto1/src/projeto1/Projeto1Teste.java
@@ -12,6 +12,44 @@ public class Projeto1Teste {
 		
 		Carro fiatUno = new Carro(proprietario, "Uno", "Preto", marca, "ABC123", 2021, 180, 5, 5, false, false, 45.0);
 		fiatUno.setConsumoPorInjecao(0.25);
+
+		//---| TESTES |---
+		System.out.println("Proprietário: "+fiatUno.getProprietario().getNome());
+		fiatUno.autonomiaEmKm();
+		
+		fiatUno.ligar();
+		fiatUno.exibirVolumeCombustivel();
+		fiatUno.acelera();
+		fiatUno.trocaMarcha();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.trocaMarcha();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.trocaMarcha();
+		fiatUno.acelera();
+		fiatUno.reduzMarcha();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.marchaRe();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.freia();
+		fiatUno.freia();
+		fiatUno.trocaMarcha();
+		fiatUno.marchaRe();
+		fiatUno.acelera();
+		fiatUno.acelera();
+		fiatUno.exibirVolumeCombustivel();
+		fiatUno.autonomiaEmKm();
+		//---
 		
 	}
 
