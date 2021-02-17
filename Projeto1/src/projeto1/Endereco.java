c2c897c55158ec8c37706305ba510185a2b718ae
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:21:56 -0300

    Criação da classe Endereco

---
+++ Projeto1/src/projeto1/Endereco.java
@@ -0,0 +1,13 @@
+package projeto1;
+
+public class Endereco {
+	
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+	private String complemento;
+
+
+}
