e89cf41816b3ae1561194e694b160f6b8e4f608f
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:32:03 -0300

    Instanciação de Marca

--- Projeto1/src/projeto1/Projeto1Teste.java
+++ Projeto1/src/projeto1/Projeto1Teste.java
@@ -7,6 +7,8 @@ public class Projeto1Teste {
 		Endereco endereco = new Endereco("Rua X, 109", "Bairro Y", "Guaratinguetá", "SP", "12509-750", "-");	
 		
 		Proprietario proprietario = new Proprietario("Fulano da Silva", "84292884064", "161265248", endereco);
+		
+		Marca marca = new Marca("Fiat", 123, 2012,"FIAT-123-2012");
 				
 	}
 
