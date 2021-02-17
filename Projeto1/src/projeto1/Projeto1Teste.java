d0214756e8d0e6d6c3420c73c9fdc6da21b454bc
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:32:50 -0300

    Instanciação de Carro e setando seu consumo por injeção

--- Projeto1/src/projeto1/Projeto1Teste.java
+++ Projeto1/src/projeto1/Projeto1Teste.java
@@ -9,7 +9,10 @@ public class Projeto1Teste {
 		Proprietario proprietario = new Proprietario("Fulano da Silva", "84292884064", "161265248", endereco);
 		
 		Marca marca = new Marca("Fiat", 123, 2012,"FIAT-123-2012");
-				
+		
+		Carro fiatUno = new Carro(proprietario, "Uno", "Preto", marca, "ABC123", 2021, 180, 5, 5, false, false, 45.0);
+		fiatUno.setConsumoPorInjecao(0.25);
+		
 	}
 
 }
