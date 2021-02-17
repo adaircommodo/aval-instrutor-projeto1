2546883f69600caec2f6663ead83fb3a5a5d7e11
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 15:16:04 -0300

    Criação da classe Proprietario e atributos solicitados

---
+++ Projeto1/src/projeto1/Proprietario.java
@@ -0,0 +1,13 @@
+package projeto1;
+
+import java.util.Date;
+
+public class Proprietario {
+	
+	private String cpf;
+	private String rg;
+	private String nome;
+	private Endereco endereco;
+	private Date dataDeNascimento;
+		
+}
